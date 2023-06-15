cf36d924dbc832e4cf16e2c7ac5b00c0d434313d
Luis Vela <luismvl@Luis>
2023-06-15 14:14:47 -0500

    import innecesario removido

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useTransition } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import styles from '../../styles/Home.module.css'
 import TodosList from './TodosList.jsx'
