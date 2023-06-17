f534145ee05c628244e21dcca8d128248465b03d
Luis Vela <luismvl@Luis>
2023-06-16 20:48:18 -0500

    test

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,26 @@
 const baseUrl = 'https://assets.breatheco.de/apis/fake/todos/user/'
 
+async function makeRequest(url, method = 'GET', body = null) {
+  try {
+    const response = await fetch(url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: body && JSON.stringify(body),
+    })
+    const data = await response.json()
+    if (!response.ok) {
+      const newError = new Error(data.msg)
+      newError.httpStatus = response.status
+      throw newError
+    }
+    return data
+  } catch (error) {
+    throw error
+  }
+}
+
 export async function getTodos(username) {
   return await makeRequest(baseUrl + username)
 }
@@ -21,25 +42,4 @@ export async function updateTodos(username, todos) {
 
 export async function deleteUser(username) {
   return await makeRequest(baseUrl + username, 'DELETE')
-}
-
-async function makeRequest(url, method = 'GET', body = null) {
-  try {
-    const response = await fetch(url, {
-      method,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: body && JSON.stringify(body),
-    })
-    const data = await response.json()
-    if (!response.ok) {
-      const newError = new Error(data.msg)
-      newError.httpStatus = response.status
-      throw newError
-    }
-    return data
-  } catch (error) {
-    throw error
-  }
 }
